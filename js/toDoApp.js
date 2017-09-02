55f27c2dfca9de10dfb74d44ebf93da258a0b2f6
smkskr <saumik.sarkar30@gmail.com>
2017-09-02 18:06:47 +0530

    To Do App in Angular JS

---
+++ js/toDoApp.js
@@ -0,0 +1,28 @@
+var toDoApp = angular.module('toDoApp', []);
+
+toDoApp.controller('toDoCtrl', function($scope) {
+	$scope.saved = localStorage.getItem('toDoList');
+	console.log($scope.saved);
+	$scope.toDoList = (localStorage.getItem('toDoList')!=null) ? JSON.parse($scope.saved): [];
+	if(localStorage.getItem('toDoList') == null)localStorage.setItem('toDoList', JSON.stringify($scope.toDoList));
+
+	
+    
+
+    $scope.toDoAdd = function() {
+    	$scope.toDoList.push({text:$scope.toDoInput,done:false});
+        $scope.toDoInput = "";
+		localStorage.setItem('toDoList', JSON.stringify($scope.toDoList));
+   };
+
+    $scope.remove = function() {
+        var oldList = $scope.toDoList;
+        localStorage.removeItem('toDoList');
+        $scope.toDoList = [];
+        angular.forEach(oldList, function(x) {
+            if (!x.done) $scope.toDoList.push({text:x.text,done:x.done});
+        });
+        localStorage.setItem('toDoList', JSON.stringify($scope.toDoList));
+    };
+    
+});
